80ebf2c56b3ae5c900ba594848b7cbb31ba7b5eb
Rodrigo Floreschapa <rodrigo.floreschapa@gmail.com>
2022-02-20 15:15:14 -0600

    Se hicieron cambios en codigo, se quito espacio antes del console.log(result)

--- js/ciclos.js
+++ js/ciclos.js
@@ -3,7 +3,6 @@ console.log('Desafio Ciclos');
 for (let i = 1; i <= 10; i++) {
     let turno = prompt('Ingresa el nombre del cliente:');
     const result = turno + ' tu No. de Turno es: ' + i
-
     console.log(result);
 }
 
