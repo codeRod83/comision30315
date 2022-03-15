a7815bc58062add687d525c8b5c2656a0b379453
Rodrigo Floreschapa <rodrigo.floreschapa@gmail.com>
2022-03-14 18:34:37 -0600

    Se agregaron botones con eventos para realizar cada una de las tareas de la pagina

--- js/interactuar-html.js
+++ js/interactuar-html.js
@@ -1,15 +1,15 @@
 console.log('DOM');
 
 // VARIABLES
-let dato0 = 0
-let proceso = 0
-const inv = [] 
+let dato0 = 4
+let proceso = 101
 const miCarrito = []
 let agregar = 's'
-let ssd = 550
-let hdd = 950
-let mouse = 680
-let monitor = 8700
+const ssd = { id: 1, nombre: 'SSD', marca: 'ADATA', costo: 550, cantidad: 10}
+const hdd = { id: 2, nombre: 'HDD', marca: 'WD', costo: 950, cantidad: 5}
+const mouse = { id: 3, nombre: 'Mouse', marca: 'Logitech', costo: 680, cantidad: 30}
+const monitor = { id: 4, nombre: 'Monitor', marca: 'Dell', costo: 8700, cantidad: 3}
+const inv = [ssd, hdd, mouse, monitor]
 let costprod = 0
 let descuento
 let desc = 0
@@ -31,10 +31,10 @@ function Productos( id, nombre, marca, costo, cantidad ) {
 function agrega() {
     agregar = prompt(`Quieres agregar otro producto al ${lugar} (s)(n)`)
 }
-function add(prod, opcion) {
-    costprod = costprod + prod
+function add(cost, opcion) {
+    costprod = costprod + cost
     console.log(`Agregaste un ${opcion} a tu carrito`)
-    console.log('   Costo: ' + prod)
+    console.log(`   Costo: $${cost}`)
     miCarrito.push(opcion)
 }
 function alerta() {
@@ -55,14 +55,14 @@ function discount(descuento) {
 }
 
 // CODIGO
-while (parseInt(proceso) <= 3) {
-     
-    proceso = prompt('Que proceso quieres realizar:\n 1) Ingresar Producto\n 2) Mostrar Inventario\n 3) Eliminar Producto\n 4) Comprar producto')
-    
-    switch (parseInt(proceso)) {
-            // AGREGA PRODUCTOS AL INVENTARIO
-        case 1:
-            agregar = 's'
+
+const boton1 = document.getElementById("btnIng")
+const boton2 = document.getElementById("btnMInv")
+const boton3 = document.getElementById("btnElim")
+const boton4 = document.getElementById("btnTienda")
+
+boton1.onclick = () => {
+    agregar = 's'
             while ( agregar != 'n' && agregar != 'N' ) {
                 
                 dato0 = dato0 + 1
@@ -77,24 +77,120 @@ while (parseInt(proceso) <= 3) {
                 lugar = 'Inventario'
                 agrega()
             }
-            break
-            // MUESTRA EL INVENTARIO INGRESADO
-        case 2: 
-            let cont = document.createElement("div")
-            cont.innerHTML = `<h1>Inventario disponible</h1>`
+    
+}
+boton2.onclick = () => {
+    let cont = document.createElement("div")
+            cont.innerHTML = `<h2>Inventario disponible</h2>`
             document.body.appendChild(cont)
             for (const producto of inv) {
                 let container = document.createElement("div")
                 container.innerHTML = `<h3>Producto ID: ${producto.id}</h3>
-                                       <p>Nombre: ${producto.nombre}</p>
-                                       <p>Marca: ${producto.marca}</p>
-                                       <b>Costo: $ ${producto.costo}</b>
-                                       <p>Disponibles: ${producto.cantidad}</p>`
+                                       <p>Nombre:       ${producto.nombre}</p>
+                                       <p>Marca:        ${producto.marca}</p>
+                                       <b>Costo:        $ ${producto.costo}</b>
+                                       <p>Disponibles:  ${producto.cantidad}</p>`
                 document.body.appendChild(container)
             }
-            console.log('Se muestra Inventario');
-            proceso = 4
-            break
+}
+boton3.onclick = () => {
+    alert('Eliminar productos\n Se encuentra mantenimiento');
+}
+boton4.onclick = () => {
+    agregar = 's'
+    prodn = inv.length
+    console.log(prodn);
+            
+            while (agregar != 'n' && agregar != 'N') {
+                let opcion = prompt(`Que producto quieres agregar:\n1 ${inv[0].nombre} \n2 ${inv[1].nombre}\n3 ${inv[2].nombre}\n4 ${inv[3].nombre} `)
+                if (opcion == 1) {
+                    add(inv[0].costo, inv[0].nombre);
+                }
+                else if (opcion == 2) {
+                    add(inv[1].costo, inv[1].nombre);
+                } else if (opcion == 3) {
+                    add(inv[2].costo, inv[2].nombre);
+                } else if (opcion == 4) {
+                    add(inv[3].costo, inv[3].nombre);
+                }
+                // miCarrito.push(`${opcion}`)
+                console.log( miCarrito.join("\n") );
+                lugar = 'Carrito de compras'
+                agrega()
+            }
+
+            alert("Los siguientes son los codigos de descuento:\n10 --> 10%\n20 --> 20%\n30 --> 30%");
+            descuento = prompt('Si tienes algun codigo de descuento ingresalo aqui: ')
+            switch (parseInt(descuento)) {
+                case 10:
+                    discount(10)
+                    break
+                case 20:
+                    discount(20)
+                    break
+                case 30:
+                    discount(30)
+                    break
+                default:
+                    break;
+            }
+            
+            sub = costprod * .84
+            let iva = costprod - sub
+    alert('Aqui tienes tu cuenta Total: ');
+    alert(`Subtotal: $ ${sub}\n        IVA: $ ${iva}\n      Total: $ ${costprod}`)
+    miCarrito.push('')
+    let prodcar = miCarrito.length - 1
+    alert(`Agregaste ${prodcar} productos a tu carrito\n${miCarrito.join(" --> 1 pza\n")}`)
+
+    
+            // console.log('Subtotal: ' + sub)
+            // console.log('     IVA: ' + iva);
+            // console.log('   Total: ' + costprod)
+            // console.log(`Agregaste ${miCarrito.length} productos a tu carrito`);
+            // miCarrito.push('')
+            // console.log(`${miCarrito.join(" --> 1 pza\n")}`);
+}
+
+
+while (parseInt(proceso) < 100) {
+     
+    switch (parseInt(proceso)) {
+            // AGREGA PRODUCTOS AL INVENTARIO
+        // case 1:
+        //     agregar = 's'
+        //     while ( agregar != 'n' && agregar != 'N' ) {
+                
+        //         dato0 = dato0 + 1
+        //         let dato1 = prompt('Que producto quieres agregar:')
+        //         let dato2 = prompt('De que marca es el producto:')
+        //         let dato3 = prompt('Que costo tendra el producto:')
+        //         let dato4 = prompt('Cuantas unidades piensas agregar:')
+                
+        //         const product = new Productos( parseInt(dato0), dato1.toUpperCase(), dato2.toUpperCase(), parseFloat(dato3), parseInt(dato4) )
+        //         console.log(product)
+        //         inv.push(product)
+        //         lugar = 'Inventario'
+        //         agrega()
+        //     }
+        //     break
+            // MUESTRA EL INVENTARIO INGRESADO
+        case 2: 
+            // let cont = document.createElement("div")
+            // cont.innerHTML = `<h2>Inventario disponible</h2>`
+            // document.body.appendChild(cont)
+            // for (const producto of inv) {
+            //     let container = document.createElement("div")
+            //     container.innerHTML = `<h3>Producto ID: ${producto.id}</h3>
+            //                            <p>Nombre:       ${producto.nombre}</p>
+            //                            <p>Marca:        ${producto.marca}</p>
+            //                            <b>Costo:        $ ${producto.costo}</b>
+            //                            <p>Disponibles:  ${producto.cantidad}</p>`
+            //     document.body.appendChild(container)
+            // }
+            // console.log('Se muestra Inventario');
+            // proceso = 100
+            // break
             // QUITA PRODUCTOS DEL INVENTARIO
         case 3:
             alert('Eliminar productos\n Se encuentra mantenimiento');
@@ -105,57 +201,60 @@ while (parseInt(proceso) <= 3) {
             break
             // CARRITO DE COMPRAS
         case 4:
-            alert('Carrito de compras\n Se encuentra mantenimiento');
-    //         agregar = 's'
-    //         console.log(`Productos a la venta:`)
-    //         for (let i = 0; i < inv.length; i++) {
-    //             let pr = i
-    //             console.log(inv[pr])
-    //         }
+            // alert('Carrito de compras\n Se encuentra mantenimiento');
+            // agregar = 's'
+            // alert(`Productos a la venta:`)
+            // for (let i = 0; i < inv.length; i++) {
+            //     let pr = i
+            //     console.log(inv[pr])
+            //     alert(inv[pr].nombre)
+            // }
             
-    //         while (agregar != 'n' && agregar != 'N') {
-    //             let opcion = prompt('Que producto quieres agregar:\n1 SSD\n2 HDD\n3 Mouse\n4 Monitor ')
-    //             if (opcion == 1) {
-    //                 add(ssd, 'SSD');
-    //             }
-    //             else if (opcion == 2) {
-    //                 add(hdd, 'HDD');
-    //             } else if (opcion == 3) {
-    //                 add(mouse, 'Mouse');
-    //             } else if (opcion == 4) {
-    //                 add(monitor, 'Monitor');
-    //             }
-    //             // miCarrito.push(`${opcion}`)
-    //             console.log( miCarrito.join("\n") );
-    //             lugar = 'Carrito de compras'
-    //             agrega()
-    //         }
+            // while (agregar != 'n' && agregar != 'N') {
+            //     console.log(inv[1].nombre);
+            //     let opcion = prompt(`Que producto quieres agregar:\n1 ${inv[1]} \n2 ${inv[2]}\n3 ${inv[3]}\n4 ${inv[4]} `)
+            //     if (opcion == 1) {
+            //         add(ssd, 'SSD');
+            //     }
+            //     else if (opcion == 2) {
+            //         add(hdd, 'HDD');
+            //     } else if (opcion == 3) {
+            //         add(mouse, 'Mouse');
+            //     } else if (opcion == 4) {
+            //         add(monitor, 'Monitor');
+            //     }
+            //     // miCarrito.push(`${opcion}`)
+            //     console.log( miCarrito.join("\n") );
+            //     lugar = 'Carrito de compras'
+            //     agrega()
+            // }
 
-    //         console.log("Los siguientes son los codigos de descuento:\n10 --> 10%\n20 --> 20%\n30 --> 30%");
-    //         descuento = prompt('Si tienes algun codigo de descuento ingresalo aqui: ')
-    //         switch (parseInt(descuento)) {
-    //             case 10:
-    //                 discount(10)
-    //                 break
-    //             case 20:
-    //                 discount(20)
-    //                 break
-    //             case 30:
-    //                 discount(30)
-    //                 break
-    //             default:
-    //                 break;
-    //         }
+            // alert("Los siguientes son los codigos de descuento:\n10 --> 10%\n20 --> 20%\n30 --> 30%");
+            // descuento = prompt('Si tienes algun codigo de descuento ingresalo aqui: ')
+            // switch (parseInt(descuento)) {
+            //     case 10:
+            //         discount(10)
+            //         break
+            //     case 20:
+            //         discount(20)
+            //         break
+            //     case 30:
+            //         discount(30)
+            //         break
+            //     default:
+            //         break;
+            // }
             
-    //         sub = costprod * .84
-    //         let iva = costprod - sub
-    //         console.log('Aqui tienes tu cuenta Total: ');
-    //         console.log('Subtotal: ' + sub)
-    //         console.log('     IVA: ' + iva);
-    //         console.log('   Total: ' + costprod)
-    //         console.log(`Agregaste ${miCarrito.length} productos a tu carrito`);
-    //         miCarrito.push('')
-    //         console.log(`${miCarrito.join(" --> 1 pza\n")}`);
+            // sub = costprod * .84
+            // let iva = costprod - sub
+            // console.log('Aqui tienes tu cuenta Total: ');
+            // console.log('Subtotal: ' + sub)
+            // console.log('     IVA: ' + iva);
+            // console.log('   Total: ' + costprod)
+            // console.log(`Agregaste ${miCarrito.length} productos a tu carrito`);
+            // miCarrito.push('')
+            // console.log(`${miCarrito.join(" --> 1 pza\n")}`);
             break
+        
     }
 }
